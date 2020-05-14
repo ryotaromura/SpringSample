429a0141393d0d04f1305f5b42423ed9c8132de2
Owner <Owner@DESKTOP-LJG35BO>
2020-05-14 18:57:41 +0900

    5/14日分の作業

---
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -0,0 +1,67 @@
+package com.example.demo.login.controller;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.login.domain.model.SignupForm;
+
+@Controller
+public class SignupController{
+
+	// ラジオボタンの実装
+	private Map<String,String>radioMarriage;
+
+	// ラジオボタンの初期化メソッド
+	private Map<String,String>initRadioMarrige() {
+
+		Map<String,String>radio = new LinkedHashMap<>();
+
+		// 既婚、未婚をMapに格納
+		radio.put("既婚","true");
+		radio.put("未婚","false");
+
+		return radio;
+	}
+
+	// ユーザー登録画面のGET用コントローラー
+	@GetMapping("/signup")
+	public String getSignUp(@ModelAttribute SignupForm form, Model model) {
+
+		// ラジオボタンの初期化メソッド呼び出し
+		radioMarriage = initRadioMarrige();
+
+		// ラジオボタン用のMapをModelに登録
+		model.addAttribute("radioMarriage", radioMarriage);
+
+		// signup.htmlに画面遷移
+		return "login/signup";
+	}
+
+	// ユーザー登録画面のPOST用コントローラー
+	@PostMapping("/signup")
+	public String postSignUp(@ModelAttribute SignupForm form,
+			BindingResult bindingResult,
+			Model model){
+
+		// データバインド失敗の場合
+		// 入力チェックに引っかかった場合」、ユーザ―登録画面に戻る
+		if(bindingResult.hasErrors()) {
+
+			// GETリクエスト用のメソッドを呼び出して、ユーザー登録画面に戻る
+			return getSignUp(form,model);
+
+		}
+
+		// formの中身をコンソールに出して確認する
+		System.out.println(form);
+
+		// login.htmlにリダイレクト
+		return "redirect:/login";
+	}
+}
---
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -0,0 +1,22 @@
+package com.example.demo.login.domain.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class SignupForm {
+
+	private String userId; //ユーザーＩＤ
+	private String password; //パスワード
+	private String userName; //ユーザー名
+
+	//@DateTimeFormat
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date birthday; //誕生日
+
+	private int age; //年齢
+	private boolean marriage; //結婚ステータス
+}

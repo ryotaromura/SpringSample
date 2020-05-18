1f4185f7f1506912537b947d69b0cdd15c16028b
Owner <Owner@DESKTOP-LJG35BO>
2020-05-18 11:43:05 +0900

    6.3.4 入力チェックのグループ実行 修正(messageとキーの紐づけ)

--- src/main/java/com/example/demo/login/domain/model/GroupOrder.java
+++ src/main/java/com/example/demo/login/domain/model/GroupOrder.java
@@ -3,8 +3,7 @@ package com.example.demo.login.domain.model;
 import javax.validation.GroupSequence;
 
 //グループの実行順序
-@GroupSequence({ValidGroup1.class,
-ValidGroup2.class, ValidGroup3.class})
-public interface GroupOrder{
+@GroupSequence({ValidGroup1.class, ValidGroup2.class, ValidGroup3.class})
+public interface GroupOrder {
 
 }

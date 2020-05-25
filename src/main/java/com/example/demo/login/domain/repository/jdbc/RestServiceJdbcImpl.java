81ba8b42a3de2188b0e523b8f90293687c2ac0d9
Owner <Owner@DESKTOP-LJG35BO>
2020-05-25 18:52:05 +0900

    5/25分の作業 10章,11-2-3

---
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/RestServiceJdbcImpl.java
@@ -0,0 +1,54 @@
+package com.example.demo.login.domain.repository.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.repository.UserDao;
+import com.example.demo.login.domain.service.RestService;
+
+@Transactional
+@Service
+public class RestServiceJdbcImpl
+implements RestService {
+
+	@Autowired
+	@Qualifier("UserDaoJdbcImpl")
+	UserDao dao;
+
+	// １件登録用メソッド
+	@Override
+	public boolean insert(User user) {
+		return false;
+	}
+
+	// １件検索用メソッド
+	@Override
+	public User selectOne(String userId) {
+		return null;
+	}
+
+	// 全件検索用メソッド
+	@Override
+	public List<User>selectMany() {
+		return null;
+	}
+
+	// １件更新用メソッド
+	@Override
+	public boolean update(User user) {
+		return false;
+	}
+
+	// １件削除用メソッド
+	@Override
+	public boolean delete(String userId) {
+		return false;
+	}
+
+
+}
--- src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.login.domain.model.User;
@@ -18,6 +19,9 @@ public class UserDaoJdbcImpl implements UserDao {
 
 	@Autowired
 	JdbcTemplate jdbc;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	// Userテーブルの件数を取得
 	@Override
@@ -33,6 +37,9 @@ public class UserDaoJdbcImpl implements UserDao {
 	@Override
 	public int insertOne(User user) throws DataAccessException {
 
+		// パスワード暗号化
+		String password = passwordEncoder.encode(user.getPassword());
+
 		// insert
 		// １件登録
 		int rowNumber = jdbc.update("INSERT INTO m_user(user_id,"
@@ -44,7 +51,7 @@ public class UserDaoJdbcImpl implements UserDao {
 		+ "role)"
 		+ "VALUES(?,?,?,?,?,?,?)"
 		,user.getUserId()
-		,user.getPassword()
+		,password
 		,user.getUserName()
 		,user.getBirthday()
 		,user.getAge()
@@ -116,6 +123,9 @@ public class UserDaoJdbcImpl implements UserDao {
 	@Override
 	public int updateOne(User user) throws DataAccessException {
 
+		// パスワード暗号化
+		String password = passwordEncoder.encode(user.getPassword());
+
 		// 一件更新
 		int rowNumber = jdbc.update("UPDATE M_USER"
 			+" SET"
@@ -125,7 +135,7 @@ public class UserDaoJdbcImpl implements UserDao {
 			+" age = ?,"
 			+" marriage = ?"
 			+" WHERE user_id = ?"
-			, user.getPassword()
+			, password
 			, user.getUserName()
 			, user.getBirthday()
 			, user.getAge()
